9f945ef91c56edd79f9fcf31975585ef7d448a57
PILsystem <amitece151@gmail.com>
2024-12-24 22:28:27 +0530

    commited all selenium explicit wait date 24122024 make appoitement

---
+++ src/Ex_2012024table/test_lab_incongnio_option.py
@@ -0,0 +1,15 @@
+import time
+
+import pytest
+import allure
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+
+def test_chrome_option():
+    chrome_options = Options()
+    chrome_options.add_argument("--incognito")
+    driver = webdriver.Chrome(chrome_options)
+    driver.get("https://www.thenavsoft.com/")
+    time.sleep(10)
