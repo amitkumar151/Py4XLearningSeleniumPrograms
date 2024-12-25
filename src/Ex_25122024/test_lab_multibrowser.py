e9e66d27408cdd04aa64e28d72ad3333beb26c7d
PILsystem <amitece151@gmail.com>
2024-12-25 13:45:52 +0530

    commited all selenium explicit wait date 25122024 url verification

--- src/Ex_25122024/test_lab_multibrowser.py
+++ src/Ex_25122024/test_lab_multibrowser.py
@@ -5,7 +5,7 @@ import allure
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.firefox.options import Options
+#from selenium.webdriver.firefox.options import Options
 
 
 def test_chrome_current_url_verification():
@@ -24,9 +24,9 @@ def test_edge_current_url_verification():
     driver.quit()
 
 
-def test_firefox_current_url_verification():
+"""def test_firefox_current_url_verification():
     driver = webdriver.Firefox()
     driver.get("https://www.needdevelopers.com/")
     assert driver.current_url == "https://www.needdevelopers.com/"
     time.sleep(7)
-    driver.quit()
+    driver.quit()"""
