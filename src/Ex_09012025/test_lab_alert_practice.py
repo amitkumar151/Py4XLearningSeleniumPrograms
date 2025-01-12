0eb9db6ea0ea627ed99ad3ecb654d1a218473c85
PILsystem <amitece151@gmail.com>
2025-01-12 13:48:25 +0530

    commited all selenium alert  date 12012025  link tag

--- src/Ex_09012025/test_lab_alert_practice.py
+++ src/Ex_09012025/test_lab_alert_practice.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 @allure.title("Alert")
 @allure.description("alert with accept")
-def test_ec_wait():
+def test_alert_normal():
     driver = webdriver.Chrome()
     driver.get("https://the-internet.herokuapp.com/javascript_alerts")
     element_promot=driver.find_element(By.XPATH,"//button[@onclick='jsAlert()']")
@@ -21,4 +21,31 @@ def test_ec_wait():
     print(result_text)
     assert result_text=="You successfully clicked an alert"
     time.sleep(3)
+def test_alert_confirm():
+    driver = webdriver.Chrome()
+    driver.get("https://the-internet.herokuapp.com/javascript_alerts")
+    element_confirm = driver.find_element(By.XPATH, "//button[@onclick='jsConfirm()']")
+    element_confirm.click()
+    WebDriverWait(driver=driver, timeout=5).until(EC.alert_is_present())
 
+    alert = driver.switch_to.alert
+    alert.dismiss()
+    result_text = driver.find_element(By.ID, "result").text
+    print(result_text)
+    assert result_text == "You clicked: Cancel"
+    time.sleep(3)
+
+def test_alert_prompt():
+    driver = webdriver.Chrome()
+    driver.get("https://the-internet.herokuapp.com/javascript_alerts")
+    element_prompt = driver.find_element(By.XPATH, "//button[@onclick='jsPrompt()']")
+    element_prompt.click()
+    WebDriverWait(driver=driver, timeout=5).until(EC.alert_is_present())
+
+    alert = driver.switch_to.alert
+    alert.send_keys("amit kumar")
+    alert.accept()
+    result_text = driver.find_element(By.ID, "result").text
+    print(result_text)
+    assert result_text == "You entered: amit kumar"
+    time.sleep(3)
