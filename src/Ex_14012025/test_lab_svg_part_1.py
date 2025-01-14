60e0db8bc1497494fc97f0fef0746a521c228876
PILsystem <amitece151@gmail.com>
2025-01-14 23:58:31 +0530

    commited all selenium svg  date 14012025  link tag

---
+++ src/Ex_14012025/test_lab_svg_part_1.py
@@ -0,0 +1,16 @@
+import allure
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+@allure.title("select")
+@allure.description("verify select")
+def test_alert_normal():
+    driver = webdriver.Chrome()
+    driver.get("https://www.flipkart.com/")
+    search_box=driver.find_element(By.XPATH, "//input[@name='q']")
+    search_box.send_keys("macmini")
+    list_svg_element=driver.find_elements(By.XPATH, "//*[name()='svg']")
+    list_svg_element[0].click()
