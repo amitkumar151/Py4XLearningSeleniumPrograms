6c2105701ee0f59d06c83bdb07620246097aeef3
PILsystem <amitece151@gmail.com>
2025-01-31 13:06:44 +0530

    commited all selenium stale JS EXECUTE  date 31012025  link tag

--- src/Ex_JS_excute/test_lab_js_p2.py
+++ src/Ex_JS_excute/test_lab_js_p2.py
@@ -10,7 +10,12 @@ from selenium.webdriver.support import expected_conditions as EC
 @allure.description("JS excute")
 def test_alert_normal():
     driver = webdriver.Chrome()
-    driver.get("https://selectorshub.com/xpath-practice-page/")
-    #driver.get("https://www.needdevelopers.com/")
+    #driver.get("https://selectorshub.com/xpath-practice-page/")
+    driver.get("https://www.needdevelopers.com/")
     driver.execute_script("window.scrollBy(0,1500)")
+    title=driver.execute_script("return document.title")
+    url=driver.execute_script("return document.URL")
+
+    print(title)
+    print(url)
     time.sleep(4)
