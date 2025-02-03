48331f1cfad91ef1ec862c7702ef3ca1d842ff31
PILsystem <amitece151@gmail.com>
2025-02-03 22:48:10 +0530

    commited all selenium js shoadow xpath url  date 03022025  link tag

--- src/Ex_JS_excute/test_lab_js_p3_scroll.py
+++ src/Ex_JS_excute/test_lab_js_p3_scroll.py
@@ -6,8 +6,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-@allure.title("JS Excute")
-@allure.description("JS excute")
+@allure.title("Shadow Dom")
+@allure.description("verify shadow Dom")
 def test_alert_normal():
     driver = webdriver.Chrome()
     driver.get("https://selectorshub.com/xpath-practice-page/")
