f44f2bf16a99c573fd1dc9907f4333325bc06250
PILsystem <amitece151@gmail.com>
2025-02-03 22:36:08 +0530

    commited all selenium js shoadow xpath url  date 03022025  link tag

---
+++ src/Ex_JS_excute/test_lab_js_p3_scroll.py
@@ -0,0 +1,22 @@
+import time
+
+import allure
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+@allure.title("JS Excute")
+@allure.description("JS excute")
+def test_alert_normal():
+    driver = webdriver.Chrome()
+    driver.get("https://selectorshub.com/xpath-practice-page/")
+    driver.maximize_window()
+    username_div=driver.find_element(By.XPATH, "//div[@id='userName']")
+    driver.execute_script("arguments[0].scrollIntoView(true);", username_div)
+    time.sleep(4)
+    input_box=driver.execute_script(
+        "return document.querySelector('div#userName').shadowRoot.querySelector('#app2').shadowRoot.querySelector('#pizza');")
+    input_box.send_keys("farmhouse")
+    time.sleep(4)
+    driver.quit()
